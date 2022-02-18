dfc2653891914f31ca86bea2758ddb89335d9e48
Alihan <72665972+alihan-ozturk@users.noreply.github.com>
2022-02-18 23:58:44 +0300

    Add files via upload

--- upDownTracker.py
+++ upDownTracker.py
@@ -1,12 +1,12 @@
 import cv2
 from tracker import EuclideanTracker
 
-paramRoot = "ssd_mobilenet_v3_large_coco_2020_01_14/frozen_inference_graph.pb"
-configRoot = "ssd_mobilenet_v3_large_coco_2020_01_14/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
-labelRoot = "ssd_mobilenet_v3_large_coco_2020_01_14/labels.txt"
-videoRoot = "test.mp4"
+paramPath = "ssd_mobilenet_v3_large_coco_2020_01_14/frozen_inference_graph.pb"
+configPath = "ssd_mobilenet_v3_large_coco_2020_01_14/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
+labelPath = "ssd_mobilenet_v3_large_coco_2020_01_14/labels.txt"
+videoPath = "test.mp4"
 
-model = cv2.dnn_DetectionModel(paramRoot, configRoot)
+model = cv2.dnn_DetectionModel(paramPath, configPath)
 model.setInputSize(320, 320)
 model.setInputScale(1.0 / 127.5)
 model.setInputMean((127.5, 127.5, 127.5))
@@ -15,13 +15,19 @@ model.setInputSwapRB(True)
 subtracktor = cv2.createBackgroundSubtractorMOG2(detectShadows=False)
 tracker = EuclideanTracker()
 
-with open(labelRoot, "rt") as l:
+with open(labelPath, "rt") as l:
     labels = l.read().rstrip("\n").split("\n")
 
 print(labels)
 
-video = cv2.VideoCapture(videoRoot)
-# writer = cv2.VideoWriter("final.avi", cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (720, 1280))
+video = cv2.VideoCapture(videoPath)
+
+frameWidth = int(video.get(3))
+frameHeight = int(video.get(4))
+
+# writer = cv2.VideoWriter('final.avi',
+#                          cv2.VideoWriter_fourcc(*'MJPG'),
+#                          10, (frameWidth, frameHeight))
 
 key = ord("q")
 
@@ -38,14 +44,16 @@ def linearEq(x):
 startPts = (0, linearEq(0))
 finishPts = (1280, linearEq(1280))
 
-ret = True
 while video.isOpened():
+
     ret, frame = video.read()
+    # if not ret:
+    #     break
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     mask = subtracktor.apply(gray)
 
-    classIndex, confidence, bbox = model.detect(frame, confThreshold=0.4)
+    classIndex, confidence, bbox = model.detect(frame, confThreshold=0.5)
 
     detections = []
     for ind, conf, bbox in zip(classIndex, confidence, bbox):
@@ -59,9 +67,9 @@ while video.isOpened():
 
         detections.append(bbox)
 
-    boxes_ids = tracker.update(detections)
-    for box_id in boxes_ids:
-        x, y, w, h, id = box_id
+    boxesIds = tracker.update(detections)
+    for boxId in boxesIds:
+        x, y, w, h, id = boxId
         cx = x + w // 2
         cy = y + h // 2
 
@@ -80,9 +88,8 @@ while video.isOpened():
     cv2.line(frame, startPts, finishPts, (255, 0, 0), 1)
 
     cv2.putText(frame, "up:{}, down:{}".format(up, down), (50, 50), font, font_scale, (0, 255, 0), 2)
-    cv2.imshow("test", frame)
     # writer.write(frame)
-
+    cv2.imshow("test", frame)
     if cv2.waitKey(1) == key:
         break
 video.release()
